d0f0b513f1dba3a80efe3477a745d627e90f724a
enclavepablo <enclavepablo@hotmail.com>
2025-10-25 18:21:42 +0000

    Initial commit: ruby project

---
+++ main.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+# asdasd - Ruby Implementation
+# A simple Hello World program
+
+def main
+  puts "Hello, World!"
+  puts "Welcome to asdasd"
+  
+  # Simple calculation
+  numbers = [1, 2, 3, 4, 5]
+  total = numbers.sum
+  puts "Sum of numbers: #{total}"
+end
+
+main
